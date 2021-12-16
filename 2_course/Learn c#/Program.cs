b5f2b24d53c4ec3123b38af9873867ab7bb18a1f
Yehor Ovseukov <ki201_oeyu@student.ztu.edu.ua>
2021-12-16 12:20:03 +0200

    initial commit

---
+++ 2_course/Learn c#/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Learn_c_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            var rand = new Random();
+
+            if(rand.Next(0, 2) == 0)
+                Console.WriteLine("Макс крутая собака !");
+            else
+                Console.WriteLine("Арчи крутая собака !");
+            
+
+        }
+    }
+}
